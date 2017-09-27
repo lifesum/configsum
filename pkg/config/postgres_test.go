882b9326a5a4431bbe9f93a495f7c2467543da2c
Alexandru Savu <alexandru@lifesum.com>
2017-09-27 14:06:14 +0200

    Expand config postgres test
    * add test targets to Makefile
    * setup postgres and repo in test

--- pkg/config/postgres_test.go
+++ pkg/config/postgres_test.go
@@ -1,9 +1,54 @@
+// +build integration
+
 package config
 
 import (
+	"flag"
+	"fmt"
+	"os/user"
 	"testing"
+
+	"github.com/jmoiron/sqlx"
+	// Blank import for Postgres capabilities.
+	_ "github.com/lib/pq"
+
+	"github.com/lifesum/configsum/pkg/pg"
 )
 
+var pgURI string
+
 func TestPostgresUserRepoGet(t *testing.T) {
-	t.Fail()
+	repo := preparePGUserRepo(t)
+
+	_, err := repo.Get("testName", "testID1234")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func preparePGUserRepo(t *testing.T) UserRepo {
+	db, err := sqlx.Connect("postgres", pgURI)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	r, err := NewPostgresUserRepo(db)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return r
+}
+
+func init() {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	uri := flag.String("postgres.uri", fmt.Sprintf(pg.DefaultTestURI, u.Username), "Postgres connection URL")
+
+	flag.Parse()
+
+	pgURI = *uri
 }
