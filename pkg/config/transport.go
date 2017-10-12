9d34da8ecd4cb05f2c7e62baa42c963bea7a792c
Alexander Simmerl <xla@tapglue.com>
2017-10-13 01:38:20 +0200

    Enable client auth for config handler
    We need a fully setup client to be present in order for the entity
    hierarchy of the config service to work properly. To acheive this we
    apply the AuthMiddleware exposed by the client package to guard against
    unathenticated calls and inject a proepr client identy in the call
    chain.
    
    * decorate user config handler with client auth middleware
    * setup client repos in main
    * setup client service in main
    * pass service to config handler

--- pkg/config/transport.go
+++ pkg/config/transport.go
@@ -2,24 +2,42 @@ package config
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/go-kit/kit/log"
 	kithttp "github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
+	"github.com/pkg/errors"
+
+	"github.com/lifesum/configsum/pkg/client"
+)
+
+// Headers.
+const (
+	headerContentType = "Content-Type"
 )
 
 // MakeHandler returns an http.Handler for the config service.
-func MakeHandler(logger log.Logger, svc ServiceUser) http.Handler {
+func MakeHandler(
+	logger log.Logger,
+	svc ServiceUser,
+	clientSVC client.Service,
+) http.Handler {
 	r := mux.NewRouter()
 	r.StrictSlash(true)
 
 	r.Methods("PUT").Path(`/{baseConfig:[a-z0-9]+}`).Name("configUser").Handler(
 		kithttp.NewServer(
-			userEndpoint(svc),
+			client.AuthMiddleware(clientSVC)(
+				userEndpoint(svc),
+			),
 			decodeUserRequest,
 			kithttp.EncodeJSONResponse,
+			kithttp.ServerBefore(client.HTTPToContext),
+			kithttp.ServerErrorEncoder(encodeError),
+			kithttp.ServerErrorLogger(log.With(logger, "route", "configUser")),
 		),
 	)
 
@@ -31,5 +49,25 @@ func decodeUserRequest(ctx context.Context, r *http.Request) (interface{}, error
 	if !ok {
 		return nil, fmt.Errorf("Baseconfig missing")
 	}
+
 	return userRequest{baseConfig: c}, nil
+}
+
+func encodeError(_ context.Context, err error, w http.ResponseWriter) {
+	switch errors.Cause(err) {
+	case ErrNotFound:
+		w.WriteHeader(http.StatusNotFound)
+	case client.ErrNotFound, client.ErrSecretMissing:
+		w.WriteHeader(http.StatusUnauthorized)
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.Header().Set(headerContentType, "application/json; charset=utf-8")
+
+	_ = json.NewEncoder(w).Encode(struct {
+		Reason string `json:"reason"`
+	}{
+		Reason: err.Error(),
+	})
 }
